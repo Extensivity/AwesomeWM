2af551e50d71d6f63c078759503d221c368f57de
Ian Bell <ianbell702@gmail.com>
2022-09-23 05:43:16 -0700

    Added a calendar to the clock.

--- widgets/top-panel/clock.lua
+++ widgets/top-panel/clock.lua
@@ -1,3 +1,9 @@
+local awful = require("awful")
 local wibox = require("wibox")
 
-return wibox.widget.textclock()
+local clock = wibox.widget.textclock()
+local calendar = awful.widget.calendar_popup.month()
+
+calendar:attach(clock, "tr")
+
+return clock
